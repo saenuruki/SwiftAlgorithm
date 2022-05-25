a42a082e6669dacce858f7e826b8b296dd11ee69
“saenuruki” <sae.webwork@gmail.com>
2022-05-25 17:59:56 +0900

    add LeetCode No.300

---
+++ LeetCode/300_Longest_Increasing_Subsequence.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        
+        var dp = Array(repeating: 1, count: nums.count)
+        var maxSubseq = 1
+    
+        for end in 1..<nums.count {
+            for start in 0..<end {
+                if nums[end] > nums[start] && dp[end] <= dp[start] {
+                    dp[end] = dp[start] + 1
+                    maxSubseq = max(maxSubseq, dp[end])
+                }
+            }
+        }
+        
+        return maxSubseq
+    }
+}
