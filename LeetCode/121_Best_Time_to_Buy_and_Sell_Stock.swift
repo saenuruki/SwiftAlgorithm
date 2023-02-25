0badc10ceb56953b320707b8428f3f28e580a055
Sae Nuruki <sae.webwork@gmail.com>
2023-02-25 15:16:52 -0500

    update LeetCode No.121

--- LeetCode/121_Best_Time_to_Buy_and_Sell_Stock.swift
+++ LeetCode/121_Best_Time_to_Buy_and_Sell_Stock.swift
@@ -1,24 +1,14 @@
-// Time: O(N)
-// Space: O(3)
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        guard prices.count > 0 else { return 0 }
-        var maxPrice = prices.first!
-        var minPrice = prices.first!
-        var output = maxPrice - minPrice
-        
-        for price in prices {
-            if price < minPrice {
-                output = max(output, maxPrice - minPrice)
-                minPrice = price
-                maxPrice = price
-            } else {
-                maxPrice = max(maxPrice, price)
-                output = max(output, maxPrice - minPrice)
-            }
+        guard prices.count > 1 else { return 0 }
+        var output = 0
+        var minimumNum = prices.first!
+
+        for i in 1..<prices.count {
+            output = max(output, prices[i] - minimumNum)
+            minimumNum = min(minimumNum, prices[i])
         }
-        
+
         return output
     }
 }
-
